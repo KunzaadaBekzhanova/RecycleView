71a7aba0f9b73a14ed14c302e3a4ab992e2d9b01
SunflowerKun <dream26322gmail.com>
2022-02-07 16:33:41 +0600

    Notification added

--- app/src/main/java/kg/tutorialapp/recycleview/MainActivity.kt
+++ app/src/main/java/kg/tutorialapp/recycleview/MainActivity.kt
@@ -1,21 +1,36 @@
 package kg.tutorialapp.recycleview
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
+    private val CHANNEL_ID = "channel_id_example_01"
+    private val notificatoinId = 101
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = CustomRecyclerAdapter(fillList())
         recyclerView.adapter = CustomRecyclerAdapter(getCatList())
+
+        createNotificationChannel()
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            sendNotification()
+        }
     }
 
     private fun fillList(): List<String> {
@@ -26,6 +41,28 @@ class MainActivity : AppCompatActivity() {
     private fun getCatList(): List<String> {
         return this.resources.getStringArray(R.array.cat_names).toList()
     }
+    private fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name = "Notification Title"
+            val descriptionText = "Notification Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+    private fun sendNotification(){
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.images)
+            .setContentTitle("Example Title")
+            .setContentText("Example Description")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
+        with(NotificationManagerCompat.from(this)){
+            notify(notificatoinId, builder.build())
+        }
+    }
 
 }
